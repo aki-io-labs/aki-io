30f6e2f489eb7060dfaeb775fcad9a8c8b3289a1
Toine <toine@aki.io>
2026-01-25 19:37:50 +0100

    relax pip dependencies to min versions

--- python/setup.py
+++ python/setup.py
@@ -7,8 +7,8 @@ setup(
     author_email='hello@aki.io',
     packages=['aki_io'],
     install_requires=[
-        'requests==2.31.0',
-        'aiohttp==3.9.0',
+        'requests >= 2.31.0',
+        'aiohttp >= 3.9.0',
     ],
     zip_safe=False
 )
